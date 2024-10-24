549b286358f937970492f4d2701f2f92e03cb4a7
Richard Black <richardb@aasteel.com>
2024-10-24 16:22:12 -0500

    Add script, initial version of whereami.sh

---
+++ whereami.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+python3 -c 'import socket; fqdn = socket.getfqdn(); print(fqdn)'
+python3 -c 'import os; hostname = os.uname().nodename; print(hostname)'
+
+hostname
+hostname -s
+hostname -f
+
+dig "$HOSTNAME" -t any +noall +answer
+dig "$HOSTNAME" +search -t any +noall +answer
+
+echo "$HOSTNAME"
+
+IP_ADDRESS=$( python3 -c 'import socket
+
+def get_local_ip():
+    try:
+        hostname = socket.gethostname()
+        local_ip = socket.gethostbyname(hostname)
+        return local_ip
+    except Exception:
+        return "Unable to get IP address"
+
+print(get_local_ip())'
+)
+
+echo "${IP_ADDRESS}"
+
+dig -x "${IP_ADDRESS}" +noall +answer
+
