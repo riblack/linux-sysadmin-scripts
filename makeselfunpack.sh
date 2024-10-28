30b1a595f0d326dac7f91fb1287c7840152f21ad
Richard Black <richardb@aasteel.com>
2024-10-28 17:49:32 -0500

    Improve script makeselfunpack, handle tar leading slash error to not affect payload

--- makeselfunpack.sh
+++ makeselfunpack.sh
@@ -10,6 +10,8 @@ makeselfunpack ()
         exit 1
     fi
 
+    OUTPUT=$( tar -czf - "$@" | base64 )
+
     cat <<'EOF'
 selfunpack () 
 { 
@@ -17,7 +19,7 @@ selfunpack ()
     cat <<'EOFTARBASE64' | base64 -d | tar -xzvf -
 EOF
 
-    tar -czf - "$@" | base64
+    echo "${OUTPUT}"
     cat <<'EOF'
 EOFTARBASE64
 }
