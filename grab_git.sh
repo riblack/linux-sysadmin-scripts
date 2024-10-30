83b787b30fdb4b69412be28102068534fb625f2d
Richard Black <richardb@aasteel.com>
2024-10-30 11:27:10 -0500

    Add script, grab_git.sh, for downloading github git repositories.

---
+++ grab_git.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+error () 
+{ 
+    printf 'ERROR: %s\n' "$@" 1>&2;
+    return 1
+}
+
+grab_git () 
+{ 
+
+    if [[ -z "$1" ]]; then
+        error "No git clone url specified.";
+        return 1;
+    fi;
+
+    git_url="$1";
+
+    mkdir -p ~/GITREPO;
+    mkdir -p ~/git;
+
+    printf '%s %s\n' "$(date "+%Y%m%d_%H%M%S")" "$git_url" >> ~/GITREPO/grab_git.log;
+
+    ( cd ~/GITREPO;
+    git clone "$git_url";
+    cd ~/git;
+    git clone "$git_url" );
+
+    current_dir=$(pwd);
+
+    case "${current_dir}" in 
+        ~/GITREPO)
+
+        ;;
+        ~/git)
+
+        ;;
+        ~/scripts)
+
+        ;;
+        ~)
+
+        ;;
+        *)
+            read -p "Do you wish to download a copy in this directory ($(pwd))? " response;
+            char=${response:0:1};
+            char=${char,,};
+
+            if [[ "$char" == "y" ]]; then
+                git clone "$git_url";
+            fi
+        ;;
+    esac
+}
+# declare -f grab_git
+grab_git "$@"
