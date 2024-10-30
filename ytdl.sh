7d8600a5ac23568569a53935be75571858232c9d
Richard Black <richardb@aasteel.com>
2024-10-30 15:18:59 -0500

    Inbetween, ytdl.sh, add subdir per domain, logging per domain

--- ytdl.sh
+++ ytdl.sh
@@ -3,7 +3,6 @@
 ytdl () 
 { 
     VIDEO_DIRECTORY=/data/videos
-    VIDEO_DOWNLOAD_URL_LOG="${VIDEO_DIRECTORY}/grab_downloaded_urls.log"
     VIDEO_DOWNLOADER=$(command -v yt-dlp)
 
     # Check if at least one argument is provided
@@ -11,9 +10,6 @@ ytdl ()
         echo "Error: No URLs provided. Please specify one or more URLs to download."
         return 1
     fi
-
-    # Create the video directory if it doesn't exist
-    [ -d "${VIDEO_DIRECTORY}" ] || mkdir -p "${VIDEO_DIRECTORY}"
 
     # Determine the name to return
     if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
@@ -32,14 +28,24 @@ ytdl ()
     while [ -n "$1" ]; do
         URL="$1"  # Get the first argument
         echo "Downloading video from: ${URL}"
-        
-        (   
-            cd "${VIDEO_DIRECTORY}" || exit 1
+
+        # Extract the domain from the URL
+        DOMAIN=$(echo "${URL}" | awk -F[/:] '{print $4}')
+        DOMAIN_DIRECTORY="${VIDEO_DIRECTORY}/${DOMAIN}"
+
+        # Create the video domain subdirectory if it doesn't exist
+        [ -d "${DOMAIN_DIRECTORY}" ] || mkdir -p "${DOMAIN_DIRECTORY}"
+
+        # Set a per domain log file
+        DOMAIN_DOWNLOAD_LOG="${DOMAIN_DIRECTORY%/}.log"
+
+        (
+            cd "${DOMAIN_DIRECTORY}" || exit 1
             DATESTAMP=$(date "+%Y-%m-%d %H:%M:%S %a")
-            printf '[%s] [%s] [%s] %s\n' "${DATESTAMP}" "${script_name}" "${HOSTNAME}" "${URL}" >> "${VIDEO_DOWNLOAD_URL_LOG}"
+            printf '[%s] [%s] [%s] %s\n' "${DATESTAMP}" "${script_name}" "${HOSTNAME}" "${URL}" >> "${DOMAIN_DOWNLOAD_LOG}"
             ${VIDEO_DOWNLOADER} "${URL}" || echo "Failed to download: ${URL}"
         )
-        
+
         shift  # Shift the arguments to process the next one
     done
 
