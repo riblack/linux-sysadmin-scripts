cb1979365a4c7b23a0cd26380d8dd272efa1126a
Richard Black <black.richard@gmail.com>
2024-10-31 01:12:08 -0500

    Add script, make_my_common_directories.sh

---
+++ make_my_common_directories.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DIRLIST=$( cat <<EOF | sed -e 's,[ \t]*\#.*$,,' | grep .
+/data/backups
+$HOME/bin
+$HOME/scripts
+EOF
+)
+
+while read dir 0<&3; do
+    [ -d "${dir}" ] || mkdir -p "${dir}"
+done 3< <( echo "$DIRLIST" )
+
+# Source footer if it exists
+[ -f "bash_footer.template.live" ] && source bash_footer.template.live || echo "Footer template missing. Skipping..."
+
