16e864cc25d5b45d6f906092a9729883cbb07f19
Richard Black <richardb@aasteel.com>
2024-11-01 09:12:50 -0500

    add install script for fsearch on ubuntu

---
+++ install_ubuntu_fsearch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+install_ubuntu_fsearch ()
+{
+    sudo add-apt-repository ppa:christian-boxdoerfer/fsearch-stable
+    before=$( sudo apt search fsearch )
+    sudo apt update
+    after=$( sudo apt search fsearch )
+    diff -Naurb <( echo "$before" ) <( echo "$after" )
+    sudo apt install fsearch
+}
+
+# Source footer if it exists
+[ -f "bash_footer.template.live" ] && source "bash_footer.template.live" || echo "Footer template missing. Skipping..."
+
