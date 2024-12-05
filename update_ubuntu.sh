affe8baab04ea9817e0c89d53ec5f94bc397dd68
Richard Black <richardb@aasteel.com>
2024-12-05 11:18:20 -0600

    Add firmware update handler to update script

--- update_ubuntu.sh
+++ update_ubuntu.sh
@@ -5,6 +5,15 @@ update_ubuntu ()
     sudo apt update -y || return 1
     apt list --upgradable
     sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo aptitude update -y && sudo aptitude upgrade -y && sudo killall snap-store && sudo snap refresh
+    package=fwupd
+    dpkg -l $package | grep -qw ^ii || sudo apt install -y $package
+    fwupdmgr refresh --force && fwupdmgr get-updates && fwupdmgr update
+    ### # Other fwupdmgr commands
+    ### fwupdmgr --help
+    ### fwupdmgr --version
+    ### fwupdmgr get-devices
+    ### # Alias to fwupdmgr update
+    ### fwupdmgr upgrade
 }
 
 # Source footer if it exists
