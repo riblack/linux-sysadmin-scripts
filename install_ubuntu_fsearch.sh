9f485331bebc5393343649526f2c66c5976ae4ad
Richard Black <richardb@aasteel.com>
2024-12-05 11:11:46 -0600

    Footer cleanup

--- install_ubuntu_fsearch.sh
+++ install_ubuntu_fsearch.sh
@@ -11,5 +11,5 @@ install_ubuntu_fsearch ()
 }
 
 # Source footer if it exists
-[ -f "bash_footer.template.live" ] && source "bash_footer.template.live" || echo "Footer template missing. Skipping..."
+[ -f "bash_footer.template.live" ] && source bash_footer.template.live || echo "Footer template missing. Skipping..."
 
