bdc05c75f20914b4c48e91566b89a369adde26eb
Richard Black <richardb@aasteel.com>
2024-12-05 11:15:20 -0600

    Convert from script to function

--- make_my_common_directories.sh
+++ make_my_common_directories.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-DIRLIST=$( cat <<EOF | sed -e 's,[ \t]*\#.*$,,' | grep .
+make_my_common_directories ()
+{
+
+    DIRLIST=$( cat <<EOF | sed -e 's,[ \t]*\#.*$,,' | grep .
 /data/backups
 $HOME/bin
 $HOME/scripts
 EOF
-)
+    )
 
-while read dir 0<&3; do
-    [ -d "${dir}" ] || mkdir -p "${dir}"
-done 3< <( echo "$DIRLIST" )
+    while read dir 0<&3; do
+        [ -d "${dir}" ] || mkdir -p "${dir}"
+    done 3< <( echo "$DIRLIST" )
+
+}
 
 # Source footer if it exists
 [ -f "bash_footer.template.live" ] && source bash_footer.template.live || echo "Footer template missing. Skipping..."
