4a1a6e6793d08e221dea73659965705edcf13dcc
Richard Black <richardb@aasteel.com>
2024-12-15 15:36:16 -0600

    Add feature request of currently mishandling semicolon on find command with trailing backslash semicolon

--- cleanup_bash_script.sh
+++ cleanup_bash_script.sh
@@ -3,6 +3,9 @@
 # Remove trailing spaces and tabs from each line.
 # Removes semicolons (;) from the end of a line if not preceded by another semicolon.
 
+# FIXME need to skip if the semicolon belongs to an if statement (preceeding by a backslash)
+# find . -maxdepth 1 -type f -name "*$(echo "${VIDEO_HANDLE}" | sed -e 's,\[,\\&,g' -e 's,\],\\&,g')*" -exec touch -d "@${FILE_DATESTAMP_EARLIEST}" {} \;
+
 sed -e 's,\([a-zA-Z_][a-zA-Z0-9_]*\)\(()\),\1 \2,' \
     -e 's,\(()\) \({\),\1\n\2,' \
     -e 's,[[:space:]]*$,,' \
