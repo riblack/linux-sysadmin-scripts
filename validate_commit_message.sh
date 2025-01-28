61856e5bb37b3f4b7203bd61c2b70b5f5a207b19
Richard Black <richardb@aasteel.com>
2025-01-28 14:58:46 -0600

    Enhance validate_commit_message.sh with color support
    - Added sourcing of load_color_codes.def for consistent color handling.
    - Improved error messages with color codes for better visibility.
    - Updated file mode from 100644 to 100755 to make the script executable.
    
    These changes enhance usability and maintain consistency with
    other scripts.

--- validate_commit_message.sh
+++ validate_commit_message.sh
@@ -3,6 +3,8 @@
 # Get the directory of the current script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+. "$SCRIPT_DIR/load_color_codes.def"
+
 # Function to validate commit message
 validate_commit_message ()
 {
@@ -44,6 +46,7 @@ validate_commit_message ()
 if [ -f "$SCRIPT_DIR/bash_footer.template.live" ]; then
     source "$SCRIPT_DIR/bash_footer.template.live"
 else
-    echo "Footer template missing. Skipping..."
+    echo -e "${red}Footer template missing. Skipping...${reset}"
+    echo -e "Please ensure 'bash_footer.template.live' exists in the same directory."
 fi
 
