20609cf2a429ceea01f762861b48f1ab46ff7fd6
Richard Black <richardb@aasteel.com>
2025-04-07 23:12:33 -0500

    shfmt formatting, Fix for set -xv
    Add case for set, so that set -xv will
    take effect. Previously it was with set -xv | sed ...
    which caused the set -xv to not stick. It now sticks.
    
    General formatting due to shfmt autoformat via vimrc

--- normal.sh
+++ normal.sh
@@ -3,14 +3,20 @@
 # Get the directory of the current script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+. "$SCRIPT_DIR/load_color_codes.def"
+
 # Normal function that only operates when debug mode is disabled
-normal ()
-{
+normal() {
     if [ "${DEBUG_MODE:=0}" -eq 0 ]; then
-        if type "$1" &> /dev/null; then
-            "$@"
+        if type "$1" &>/dev/null; then
+            if [[ "$1" == "set" ]]; then
+                # Run 'set' command bare so options like -xv take effect
+                "$@"
+            else
+                "$@" 2>&1 | sed -e "s,^,DEBUG: ," 1>&2
+            fi
         else
-            echo "$@" 1>&2
+            echo "$*" 1>&2
         fi
     fi
 }
@@ -19,6 +25,6 @@ normal ()
 if [ -f "$SCRIPT_DIR/bash_footer.template.live" ]; then
     source "$SCRIPT_DIR/bash_footer.template.live"
 else
-    echo "Footer template missing. Skipping..."
+    echo -e "${RED}Footer template missing. Skipping...${RESET}"
+    echo -e "Please ensure 'bash_footer.template.live' exists in the same directory."
 fi
-
