e3ea2076eb31da1ef7957947bd5efce17287c405
Richard Black <richardb@aasteel.com>
2025-04-23 09:57:00 -0500

    Allow for zim yyyy/mm/dd if it exists w/ fallbacks
    In order attempt the following:
    zim notebook dir / Journal / yyyy / mm / dd
    zim notebook dir / Journal / yyyy / mm
    zim notebook dir / Journal / yyyy
    zim notebook dir / Journal
    zim notebook dir

--- cdz.sh
+++ cdz.sh
@@ -4,8 +4,22 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cdz() {
+    ZIM_BASE_DIR="${HOME}/Notebooks/Notes/Journal"
+    JOURNAL="Journal"
+    NOW_YEAR=$(date +%Y)
+    NOW_MONTH=$(date +%Y/%m)
+    NOW_DAY=$(date +%Y/%m/%d)
     ZIM_DIR="${HOME}/Notebooks/Notes/Journal/$(date +%Y/%m)"
-    [ -d "${ZIM_DIR}" ] && cd "${ZIM_DIR}"
+    ZIM_DIR="${ZIM_BASE_DIR}/$NOW_DAY"
+    [ -d "${ZIM_DIR}" ] && cd "${ZIM_DIR}" && return
+    ZIM_DIR="${ZIM_BASE_DIR}/$NOW_MONTH"
+    [ -d "${ZIM_DIR}" ] && cd "${ZIM_DIR}" && return
+    ZIM_DIR="${ZIM_BASE_DIR}/$NOW_YEAR"
+    [ -d "${ZIM_DIR}" ] && cd "${ZIM_DIR}" && return
+    ZIM_DIR="${ZIM_BASE_DIR}/$JOURNAL"
+    [ -d "${ZIM_DIR}" ] && cd "${ZIM_DIR}" && return
+    ZIM_DIR="${ZIM_BASE_DIR}"
+    [ -d "${ZIM_DIR}" ] && cd "${ZIM_DIR}" && return
 }
 
 # Source footer if it exists
